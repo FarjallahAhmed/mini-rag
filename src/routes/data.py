191ab7d608be24eb948ced92669752dbcdad13c8
FarjallahAhmed <ahmed.farjallah@esprit.tn>
2024-09-29 19:47:51 +0100

    add assets to control project and their files

--- src/routes/data.py
+++ src/routes/data.py
@@ -10,6 +10,9 @@ from models.ChunkModel import ChunkModel
 import logging
 from .schemes.data import ProcessRequest
 from models.db_schemes import DataChunk
+from models.AssetModel import AssetModel
+from models.db_schemes import DataChunk, Asset
+from models.enums.AssetTypeEnum import AssetTypeEnum
 
 logger = logging.getLogger("uvicorn.error")
 router_data = APIRouter(
@@ -51,12 +54,27 @@ async def upload_data(request: Request,project_id: str, file: UploadFile, app_se
                 "signal" : ResponseSignal.FILE_UPLOAD_ERROR.value
             }
         )
+    
+     # store the assets into the database
+    asset_model = await AssetModel.create_instance(
+        db_client=request.app.db_client
+    )
+
+    asset_resource = Asset(
+        asset_project_id=project.id,
+        asset_type=AssetTypeEnum.FILE.value,
+        asset_name=file_id,
+        asset_size=os.path.getsize(file_path)
+    )
+
+    asset_record = await asset_model.create_asset(asset=asset_resource)
 
     return JSONResponse(
         content={
             "signal" : ResponseSignal.FILE_UPLOAD_SUCCESS.value,
             "file_id": file_id,
-            "project_id": str(project.id)
+            "project_id": str(project.id),
+            "asset_id": str(asset_record.id)
         }
     )
 
